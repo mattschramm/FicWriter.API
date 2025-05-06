e711090e78d7a2883439eeb2d8bc5a42b9071f37
Matheus Schramm <mathschramm@outlook.com>
2025-05-06 17:37:03 -0400

    chore: updated migrations

---
+++ FicWriter.API/Enums/Genres.cs
@@ -0,0 +1,19 @@
+﻿namespace FicWriter.API.Enums;
+
+public enum Genres
+{
+    None = 0,
+    Action = 1,
+    Adventure = 2,
+    Romance = 3,
+    Fantasy = 4,
+    SciFi = 5,
+    Horror = 6,
+    Mystery = 7,
+    Thriller = 8,
+    Historical = 9,
+    Comedy = 10,
+    Drama = 11,
+    NoFiction = 12,
+    Poetry = 13
+}
---
+++ FicWriter.API/Features/Works/Dashboard/GetDashboardCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using ErrorOr;
+using MediatR;
+
+namespace FicWriter.API.Features.Works.Dashboard;
+
+public record GetDashboardCommand() : IRequest<ErrorOr<GetDashboardResponse>>;
+
+public record GetDashboardResponse(string Id, string Title, string ShortDescription, DateTime UpdatedAt);
+
+/*public class GetDashboardCommandHandler : IRequestHandler<GetDashboardCommand, ErrorOr<GetDashboardResponse>>
+{
+    public Task<ErrorOr<GetDashboardResponse>> Handle(GetDashboardCommand request, CancellationToken cancellationToken) => throw new NotImplementedException();
+}*/
